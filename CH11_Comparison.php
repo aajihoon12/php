6d83f0b0b1b59bcaf952d919e4629dd1d25a7213
JihoonLee <aajihoon12@gmail.com>
2021-01-14 00:17:52 +0900

    CH12

---
+++ CH11_Comparison.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset='uft-8'>
+        <title></title>
+    </head>
+    <body>
+        <h1>Comparison Operators & Boolean data type</h1>
+        <?php
+            var_dump(11);
+            var_dump('11');
+            var_dump(1+1);
+            var_dump(1==1);
+            var_dump(1>1);
+            var_dump(1>=1);
+        ?>
+    </body>
+</html>
---
+++ CH12_Conditional.php
@@ -0,0 +1,24 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset='uft-8'>
+        <title></title>
+    </head>
+    <body>
+        <h1>Conditional</h1>
+        <?php
+            echo '1<br>';
+            echo '2<br>';
+            echo '3<br>';
+        ?>
+        <h2>if</h2>
+        <?php
+            echo '1<br>';
+            if(0) {
+                echo '2<br>';
+            } else {
+                echo '3<br>';
+            } 
+        ?>
+    </body>
+</html>
